26357ea0a82d46918b30e0813cb6fef23e83a3db
Irina <irinakhaus@gmail.com>
2024-05-26 22:14:24 +0300

    Changed to classes

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
 import vk_api
 import json
 import os
-import urllib
 import urllib.request
 import ssl 
 from configuration import config
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-token = config.token.get_secret_value()
 
-owner_id = -220740378
-
-vk_session = vk_api.VkApi(token=token)
-vk = vk_session.get_api()
-
-photos = vk.photos.get(owner_id=owner_id, album_id='wall', photo_sizes=1, count=200)
-count = 0
-
-images_path = 'images'
-if not os.path.exists(images_path):
-    os.makedirs(images_path)
-
-path = os.getcwd() + '/images'
-
-for photo in photos['items']:
-    max_size_photo = sorted(photo['sizes'], key=lambda dict: dict['height'])
-    url = max_size_photo[-1]['url']
-    name = f'{path}/{count}.jpg' 
-    urllib.request.urlretrieve(url, name)
-    count += 1
+class Parser():
     
-# with open('data.json', 'w') as fp:
-#     fp.write(json.dumps(photos))
+    def __init__(self):
+        self.token = config.token.get_secret_value()
+        self.vk_session = vk_api.VkApi(token=self.token)
+        self.vk = self.vk_session.get_api()
+        self.count = 0
+        self.offset = 0
+        
+    def parsing(self, owner_id=-220740378):
+        images_folder = 'images'
+        if not os.path.exists(images_folder):
+            os.makedirs(images_folder)
+        path = os.getcwd() + '/' + images_folder
+        
+        while True:
+            images = self.vk.photos.get(owner_id=owner_id, album_id='wall', photo_sizes=1, count=1000, offset=self.offset)
+            for image in images['items']:
+                max_size_photo = sorted(image['sizes'], key=lambda dict: dict['height'])
+                url = max_size_photo[-1]['url']
+                name = f'{path}/{self.count}.jpg' 
+                urllib.request.urlretrieve(url, name)
+                self.count += 1
+            self.offset += 1000
+            if self.offset >= images['count']:
+                break
+            
+
+    
+        # with open('data.json', 'w') as fp:
+        #     fp.write(json.dumps(photos))
+        
+
+def main():
+    parser = Parser()
+    parser.parsing()
+    
+
+if __name__ == "__main__":
+    main()
