11531a80c4b0d8ce6b10b4324fcf930953495480
Irina <irinakhaus@gmail.com>
2024-06-05 19:35:18 +0300

    Change tabs objects name

--- ui/main_window_ui.py
+++ ui/main_window_ui.py
@@ -24,7 +24,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(439, 252)
+        MainWindow.resize(433, 252)
         MainWindow.setMinimumSize(QSize(0, 0))
         MainWindow.setMaximumSize(QSize(3900, 2410))
         MainWindow.setStyleSheet(u"background-color: #edeef0;\n"
@@ -36,9 +36,9 @@ class Ui_MainWindow(object):
         self.verticalLayout_9.setSpacing(0)
         self.verticalLayout_9.setObjectName(u"verticalLayout_9")
         self.verticalLayout_9.setContentsMargins(6, 6, 6, 6)
-        self.params_tabWidget = QTabWidget(self.centralwidget)
-        self.params_tabWidget.setObjectName(u"params_tabWidget")
-        self.params_tabWidget.setStyleSheet(u"QTabWidget::pane {\n"
+        self.params_tabwidget = QTabWidget(self.centralwidget)
+        self.params_tabwidget.setObjectName(u"params_tabwidget")
+        self.params_tabwidget.setStyleSheet(u"QTabWidget::pane {\n"
 "  border: 1px solid lightgray;\n"
 "  top:-1px; \n"
 "  background: rgb(245, 245, 245); \n"
@@ -54,24 +54,24 @@ class Ui_MainWindow(object):
 "  background: white; \n"
 "  margin-bottom: -1px; \n"
 "}")
-        self.params_tabWidgetPage1 = QWidget()
-        self.params_tabWidgetPage1.setObjectName(u"params_tabWidgetPage1")
-        self.params_tabWidgetPage1.setStyleSheet(u"border: none;\n"
+        self.page_photos = QWidget()
+        self.page_photos.setObjectName(u"page_photos")
+        self.page_photos.setStyleSheet(u"border: none;\n"
 "background: white;")
-        self.verticalLayout_5 = QVBoxLayout(self.params_tabWidgetPage1)
+        self.verticalLayout_5 = QVBoxLayout(self.page_photos)
         self.verticalLayout_5.setSpacing(2)
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
         self.verticalLayout_5.setContentsMargins(6, 6, 6, 6)
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_group_id = QLabel(self.params_tabWidgetPage1)
+        self.label_group_id = QLabel(self.page_photos)
         self.label_group_id.setObjectName(u"label_group_id")
         self.label_group_id.setStyleSheet(u"background-color: transparent;\n"
 "padding-right: 2px;")
 
         self.horizontalLayout_2.addWidget(self.label_group_id)
 
-        self.line_edit_group_id = QLineEdit(self.params_tabWidgetPage1)
+        self.line_edit_group_id = QLineEdit(self.page_photos)
         self.line_edit_group_id.setObjectName(u"line_edit_group_id")
         self.line_edit_group_id.setMinimumSize(QSize(0, 0))
         self.line_edit_group_id.setMaximumSize(QSize(16777215, 16777215))
@@ -98,7 +98,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_6.setSpacing(6)
         self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
         self.horizontalLayout_6.setContentsMargins(-1, 10, -1, -1)
-        self.label_select_sources = QLabel(self.params_tabWidgetPage1)
+        self.label_select_sources = QLabel(self.page_photos)
         self.label_select_sources.setObjectName(u"label_select_sources")
         self.label_select_sources.setMinimumSize(QSize(0, 0))
         self.label_select_sources.setStyleSheet(u"background-color: transparent;\n"
@@ -114,7 +114,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setSpacing(0)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.check_box_wall = QCheckBox(self.params_tabWidgetPage1)
+        self.check_box_wall = QCheckBox(self.page_photos)
         self.check_box_wall.setObjectName(u"check_box_wall")
         self.check_box_wall.setMaximumSize(QSize(16777215, 16777215))
         self.check_box_wall.setCursor(QCursor(Qt.PointingHandCursor))
@@ -141,7 +141,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.check_box_wall)
 
-        self.button_wall_settings = QPushButton(self.params_tabWidgetPage1)
+        self.button_wall_settings = QPushButton(self.page_photos)
         self.button_wall_settings.setObjectName(u"button_wall_settings")
         self.button_wall_settings.setEnabled(False)
         self.button_wall_settings.setMaximumSize(QSize(24, 24))
@@ -173,7 +173,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.check_box_album = QCheckBox(self.params_tabWidgetPage1)
+        self.check_box_album = QCheckBox(self.page_photos)
         self.check_box_album.setObjectName(u"check_box_album")
         self.check_box_album.setEnabled(True)
         self.check_box_album.setMaximumSize(QSize(16777215, 16777215))
@@ -202,7 +202,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4.addWidget(self.check_box_album)
 
-        self.button_album_settings = QPushButton(self.params_tabWidgetPage1)
+        self.button_album_settings = QPushButton(self.page_photos)
         self.button_album_settings.setObjectName(u"button_album_settings")
         self.button_album_settings.setEnabled(False)
         self.button_album_settings.setMaximumSize(QSize(24, 24))
@@ -240,7 +240,7 @@ class Ui_MainWindow(object):
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_select_path = QLabel(self.params_tabWidgetPage1)
+        self.label_select_path = QLabel(self.page_photos)
         self.label_select_path.setObjectName(u"label_select_path")
         self.label_select_path.setStyleSheet(u"background-color: transparent;\n"
 "padding-right: 2px;")
@@ -251,7 +251,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setSpacing(0)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.line_edit_select_path = QLineEdit(self.params_tabWidgetPage1)
+        self.line_edit_select_path = QLineEdit(self.page_photos)
         self.line_edit_select_path.setObjectName(u"line_edit_select_path")
         self.line_edit_select_path.setEnabled(False)
         self.line_edit_select_path.setMinimumSize(QSize(0, 0))
@@ -272,7 +272,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_5.addWidget(self.line_edit_select_path)
 
-        self.button_select_path = QPushButton(self.params_tabWidgetPage1)
+        self.button_select_path = QPushButton(self.page_photos)
         self.button_select_path.setObjectName(u"button_select_path")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -313,7 +313,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.button_parse = QPushButton(self.params_tabWidgetPage1)
+        self.button_parse = QPushButton(self.page_photos)
         self.button_parse.setObjectName(u"button_parse")
         self.button_parse.setEnabled(True)
         self.button_parse.setCursor(QCursor(Qt.PointingHandCursor))
@@ -338,7 +338,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addWidget(self.button_parse)
 
-        self.label_info = QLabel(self.params_tabWidgetPage1)
+        self.label_info = QLabel(self.page_photos)
         self.label_info.setObjectName(u"label_info")
         self.label_info.setStyleSheet(u"background-color: transparent;")
         self.label_info.setAlignment(Qt.AlignBottom|Qt.AlignLeading|Qt.AlignLeft)
@@ -348,12 +348,12 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_5.addLayout(self.verticalLayout_3)
 
-        self.params_tabWidget.addTab(self.params_tabWidgetPage1, "")
-        self.tab_3 = QWidget()
-        self.tab_3.setObjectName(u"tab_3")
-        self.tab_3.setStyleSheet(u"border: none;\n"
+        self.params_tabwidget.addTab(self.page_photos, "")
+        self.page_authorization = QWidget()
+        self.page_authorization.setObjectName(u"page_authorization")
+        self.page_authorization.setStyleSheet(u"border: none;\n"
 "background: white;")
-        self.verticalLayout_7 = QVBoxLayout(self.tab_3)
+        self.verticalLayout_7 = QVBoxLayout(self.page_authorization)
         self.verticalLayout_7.setSpacing(0)
         self.verticalLayout_7.setObjectName(u"verticalLayout_7")
         self.verticalLayout_7.setContentsMargins(6, 6, 6, 6)
@@ -361,7 +361,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_8.setSpacing(0)
         self.verticalLayout_8.setObjectName(u"verticalLayout_8")
         self.verticalLayout_8.setContentsMargins(-1, -1, -1, 0)
-        self.label_token_info = QLabel(self.tab_3)
+        self.label_token_info = QLabel(self.page_authorization)
         self.label_token_info.setObjectName(u"label_token_info")
         self.label_token_info.setStyleSheet(u"background-color: transparent;")
 
@@ -370,7 +370,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_7 = QHBoxLayout()
         self.horizontalLayout_7.setSpacing(0)
         self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
-        self.line_edit_token = QLineEdit(self.tab_3)
+        self.line_edit_token = QLineEdit(self.page_authorization)
         self.line_edit_token.setObjectName(u"line_edit_token")
         self.line_edit_token.setMinimumSize(QSize(0, 0))
         self.line_edit_token.setStyleSheet(u"QLineEdit {\n"
@@ -390,7 +390,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_7.addWidget(self.line_edit_token)
 
-        self.button_save = QPushButton(self.tab_3)
+        self.button_save = QPushButton(self.page_authorization)
         self.button_save.setObjectName(u"button_save")
         self.button_save.setCursor(QCursor(Qt.PointingHandCursor))
         self.button_save.setStyleSheet(u"QPushButton {\n"
@@ -419,7 +419,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_8.addLayout(self.horizontalLayout_7)
 
-        self.label = QLabel(self.tab_3)
+        self.label = QLabel(self.page_authorization)
         self.label.setObjectName(u"label")
         self.label.setStyleSheet(u"font-weight: bold;")
         self.label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
@@ -430,13 +430,13 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setSpacing(7)
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.verticalLayout_4.setContentsMargins(-1, -1, -1, 5)
-        self.label_step_1 = QLabel(self.tab_3)
+        self.label_step_1 = QLabel(self.page_authorization)
         self.label_step_1.setObjectName(u"label_step_1")
         self.label_step_1.setStyleSheet(u"")
 
         self.verticalLayout_4.addWidget(self.label_step_1)
 
-        self.label_step_4 = QLabel(self.tab_3)
+        self.label_step_4 = QLabel(self.page_authorization)
         self.label_step_4.setObjectName(u"label_step_4")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
@@ -446,7 +446,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_4.addWidget(self.label_step_4)
 
-        self.label_2 = QLabel(self.tab_3)
+        self.label_2 = QLabel(self.page_authorization)
         self.label_2.setObjectName(u"label_2")
 
         self.verticalLayout_4.addWidget(self.label_2)
@@ -457,7 +457,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.line_edit_client_id = QLineEdit(self.tab_3)
+        self.line_edit_client_id = QLineEdit(self.page_authorization)
         self.line_edit_client_id.setObjectName(u"line_edit_client_id")
         self.line_edit_client_id.setMinimumSize(QSize(0, 21))
         self.line_edit_client_id.setStyleSheet(u"QLineEdit {\n"
@@ -477,7 +477,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.line_edit_client_id)
 
-        self.button_get_token = QPushButton(self.tab_3)
+        self.button_get_token = QPushButton(self.page_authorization)
         self.button_get_token.setObjectName(u"button_get_token")
         self.button_get_token.setCursor(QCursor(Qt.PointingHandCursor))
         self.button_get_token.setStyleSheet(u"QPushButton {\n"
@@ -506,7 +506,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_8.addLayout(self.horizontalLayout)
 
-        self.label_step_5 = QLabel(self.tab_3)
+        self.label_step_5 = QLabel(self.page_authorization)
         self.label_step_5.setObjectName(u"label_step_5")
         self.label_step_5.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
 
@@ -515,15 +515,15 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_7.addLayout(self.verticalLayout_8)
 
-        self.params_tabWidget.addTab(self.tab_3, "")
+        self.params_tabwidget.addTab(self.page_authorization, "")
 
-        self.verticalLayout_9.addWidget(self.params_tabWidget)
+        self.verticalLayout_9.addWidget(self.params_tabwidget)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
-        self.params_tabWidget.setCurrentIndex(1)
+        self.params_tabwidget.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -541,7 +541,7 @@ class Ui_MainWindow(object):
         self.button_select_path.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u0442\u044c", None))
         self.button_parse.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u0430\u0447\u0430\u0442\u044c", None))
         self.label_info.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0434\u0441\u043a\u0430\u0437\u043a\u0430: \u0437\u0430\u043f\u043e\u043b\u043d\u0438\u0442\u0435 \u0432\u0441\u0435 \u043f\u043e\u043b\u044f \u0438 \u043d\u0430\u0436\u043c\u0438\u0442\u0435 \u043a\u043d\u043e\u043f\u043a\u0443 \u00ab\u0421\u043a\u0430\u0447\u0430\u0442\u044c\u00bb", None))
-        self.params_tabWidget.setTabText(self.params_tabWidget.indexOf(self.params_tabWidgetPage1), QCoreApplication.translate("MainWindow", u"\u0424\u043e\u0442\u043e\u0433\u0440\u0430\u0444\u0438\u0438", None))
+        self.params_tabwidget.setTabText(self.params_tabwidget.indexOf(self.page_photos), QCoreApplication.translate("MainWindow", u"\u0424\u043e\u0442\u043e\u0433\u0440\u0430\u0444\u0438\u0438", None))
         self.label_token_info.setText(QCoreApplication.translate("MainWindow", u"\u0422\u043e\u043a\u0435\u043d \u043d\u0435 \u0434\u0435\u0439\u0441\u0442\u0432\u0438\u0442\u0435\u043b\u0435\u043d", None))
         self.button_save.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c \u0442\u043e\u043a\u0435\u043d", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043b\u044f \u043f\u043e\u043b\u0443\u0447\u0435\u043d\u0438\u044f \u043d\u043e\u0432\u043e\u0433\u043e \u0442\u043e\u043a\u0435\u043d\u0430:", None))
@@ -550,6 +550,6 @@ class Ui_MainWindow(object):
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"3. \u0412\u0432\u0435\u0434\u0438\u0442\u0435 ID \u043f\u0440\u0438\u043b\u043e\u0436\u0435\u043d\u0438\u044f \u0432 \u043f\u043e\u043b\u0435 \u043d\u0438\u0436\u0435 \u0438 \u043f\u0435\u0440\u0435\u0439\u0434\u0438\u0442\u0435 \u043f\u043e \u0441\u0441\u044b\u043b\u043a\u0435.", None))
         self.button_get_token.setText(QCoreApplication.translate("MainWindow", u"\u041f\u0435\u0440\u0435\u0439\u0442\u0438 \u043f\u043e \u0441\u0441\u044b\u043b\u043a\u0435", None))
         self.label_step_5.setText(QCoreApplication.translate("MainWindow", u"4. \u0421\u043a\u043e\u043f\u0438\u0440\u0443\u0439\u0442\u0435 \u0437\u043d\u0430\u0447\u0435\u043d\u0438\u0435 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440\u0430 access_token \u0438\u0437 \u0430\u0434\u0440\u0435\u0441\u043d\u043e\u0439 \u0441\u0442\u0440\u043e\u043a\u0438.", None))
-        self.params_tabWidget.setTabText(self.params_tabWidget.indexOf(self.tab_3), QCoreApplication.translate("MainWindow", u"\u0410\u0432\u0442\u043e\u0440\u0438\u0437\u0430\u0446\u0438\u044f", None))
+        self.params_tabwidget.setTabText(self.params_tabwidget.indexOf(self.page_authorization), QCoreApplication.translate("MainWindow", u"\u0410\u0432\u0442\u043e\u0440\u0438\u0437\u0430\u0446\u0438\u044f", None))
     # retranslateUi
 
